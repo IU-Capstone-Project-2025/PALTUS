8fbfb4372e53f7a9a48b17629a258c3013535a69
Igor0006 <igordubrovskij69@gmail.com>
2025-06-11 16:06:15 +0300

    build: add Dockerfile for backend

--- backend/src/main/java/com/paltus/backend/controller/UserController.java
+++ backend/src/main/java/com/paltus/backend/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.paltus.backend.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class UserController {
+
+    public UserController() { }
+    @GetMapping("/hello")
+    public String getMethodName() {
+        return "Hello test";
+    }
     
 }
