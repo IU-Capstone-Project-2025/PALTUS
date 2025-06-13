bda30c900518b3fc8197a2859de3fee2d106b7d3
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-06-13 21:25:04 +0300

    feat: add authorization with mocks

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,32 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue';
+import {useAuthStore} from "@/stores/auth.js";
 
 const routes = [
-    { path: '/', component: HomeView },
+    { path: '/login', component: LoginView },
+    {
+        path: '/',
+        component: HomeView,
+        meta: { requiresAuth: true }
+    },
 ]
 
 const router = createRouter({
     history: createMemoryHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    const auth = useAuthStore();
+    auth.loadUser(); // загружаем из localStorage
+
+    if (to.meta.requiresAuth && !auth.isAuthenticated()) {
+        next('/login');
+    } else {
+        next();
+    }
+});
 
 export default router
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
+const email = ref('');
+const password = ref('');
+const auth = useAuthStore();
+const router = useRouter();
+
+async function loginUser() {
+  try {
+    await auth.login(email.value, password.value);
+    await router.push('/');
+  } catch (err) {
+    alert(err.message);
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="loginUser">
+    <input v-model="email" placeholder="Email" />
+<!--    test@example.com-->
+    <input v-model="password" type="password" placeholder="Password" />
+<!--    123-->
+    <button type="submit">Log In</button>
+  </form>
+</template>
