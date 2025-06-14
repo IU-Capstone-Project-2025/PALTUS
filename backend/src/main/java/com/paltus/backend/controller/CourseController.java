85ddc9723916b237dacaf0caa6dc3f0fdba1ddc5
Aidar <aidarsarvartdinov2004@gmail.com>
2025-06-14 19:14:39 +0300

    feat(course): add getAllCoursesSummaries method to get list of course ids and names

--- backend/src/main/java/com/paltus/backend/controller/CourseController.java
+++ backend/src/main/java/com/paltus/backend/controller/CourseController.java
@@ -1,11 +1,14 @@
 package com.paltus.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.paltus.backend.dto.CourseSummaryDto;
 import com.paltus.backend.model.Course;
 import com.paltus.backend.service.CourseService;
 
@@ -18,6 +21,11 @@ public class CourseController {
     @Autowired
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
+    }
+
+    @GetMapping()
+    public List<CourseSummaryDto> getAllCoursesSummaries() {
+        return courseService.getAllCoursesSummaries();
     }
 
     @GetMapping("/{id}")
