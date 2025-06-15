d9a3549613b74e382515f834289219e0f8fee85b
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-06-15 17:23:03 +0300

    style: add layout for course creation page

---
+++ frontend/src/components/shared/BaseHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <h1>
+    {{ text }}
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #42A5F5;
+  font-size: 24px;
+  padding: 0.5vh 0;
+  font-weight: bold;
+}
+</style>
---
+++ frontend/src/views/CourseCreationView.vue
@@ -0,0 +1,103 @@
+<script setup>
+import Logo from "@/components/shared/Logo.vue";
+import MyCourses from "@/components/shared/MyCourses.vue";
+import Account from "@/components/home/Account.vue";
+import BaseHeader from "@/components/shared/BaseHeader.vue";
+import BaseInput from "@/components/shared/BaseInput.vue";
+import {ref} from "vue";
+import ButtonGreen from "@/components/shared/ButtonGreen.vue";
+
+const props = defineProps({
+  courseName: String,
+})
+
+const name = ref('');
+const currentSkills = ref('');
+const goal = ref('');
+const lessonsNum = ref('');
+const durability = ref('');
+
+const inputs = ref([
+      {
+        question: 'Create a new course:',
+        placeholder: 'Course topic...',
+        model: name,
+      }, {
+        question: 'What is your current knowledge about this topic?',
+        placeholder: 'I know...',
+        model: currentSkills,
+      }, {
+        question: 'What is your goal?',
+        placeholder: 'I want to learn...',
+        model: goal,
+      }, {
+        question: 'How many lessons and which durability for a lesson?',
+        placeholder: 'Number of lessons',
+        model: lessonsNum,
+      }
+    ]);
+</script>
+
+<template>
+  <div class="main">
+    <section class="left">
+      <Logo />
+      <MyCourses />
+    </section>
+    <section class="center">
+      <div
+          class="question"
+          v-for="inp in inputs"
+      >
+        <BaseHeader :text="inp.question" class="header"/>
+        <BaseInput :placeholder="inp.placeholder" v-model="inp.model"/>
+      </div>
+      <div class="question" style="margin-top: 1vh">
+        <BaseInput placeholder="Durability of a lesson" v-model="durability"/>
+      </div>
+      <div class="get-course">
+        <ButtonGreen title="GET A COURSE" />
+      </div>
+    </section>
+    <section class="right">
+      <Account />
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+}
+
+.left {
+  width: 30vw
+}
+
+.center {
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  align-items: center;
+  margin-top: 2vh;
+}
+
+.question {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 20vw;
+  margin-top: 3vh;
+}
+
+.right {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 30vw;
+}
+
+.get-course {
+  margin-top: 3vh;
+}
+</style>
