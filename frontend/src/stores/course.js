1fb2aceca2d5c15d31a8910a0e0a6e8e7b54b2bd
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-06-24 02:48:44 +0300

    chore: add store for a course

--- frontend/src/stores/course.js
+++ frontend/src/stores/course.js
@@ -1,2 +1,24 @@
 import { defineStore } from "pinia";
+import axios from "axios";
 
+export const useCourseStore = defineStore("course", {
+    state: () => ({
+        course_name: '',
+        description: '',
+        books: [],
+        lessons: [],
+    }),
+    actions: {
+        async loadCourse(id) {
+            try {
+                const response = await axios.get(`/api/courses/${id}`);
+                this.course_name = response.course_name;
+                this.description = response.description;
+                this.books = response.books;
+                this.lessons = response.lessons;
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+})
