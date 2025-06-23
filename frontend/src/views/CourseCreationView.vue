7dc93b718e3ceace5ea73d48ed83a435c98dfbbe
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-06-23 15:28:31 +0300

    feat: add requests for adding a course

--- frontend/src/views/CourseCreationView.vue
+++ frontend/src/views/CourseCreationView.vue
@@ -4,10 +4,11 @@ import MyCourses from "@/components/shared/MyCourses.vue";
 import Account from "@/components/shared/Account.vue";
 import BaseHeader from "@/components/shared/BaseHeader.vue";
 import BaseInput from "@/components/shared/BaseInput.vue";
-import {ref} from "vue";
 import ButtonGreen from "@/components/shared/ButtonGreen.vue";
+import {ref} from "vue";
 import router from "@/router/index.js";
 import { useRoute } from 'vue-router';
+import axios from "@/plugins/axios.js"
 
 const route = useRoute();
 const name = ref(route.query.courseName || '');
@@ -45,19 +46,32 @@ const validation = () => {
       && goal.value.length > 0
       && lessonsNumInt
       && durationInt
-      && lessonsNumInt > 5
-      && lessonsNumInt < 15
-      && durationInt > 10
-      && durationInt < 240) {
+      && lessonsNumInt >= 5
+      && lessonsNumInt <= 16
+      && durationInt >= 20
+      && durationInt <= 240) {
     return 1;
   }
   return 0;
 }
 
-const getCourse = /* async */ () => {
+const getCourse = async () => {
   if (validation()) {
-    // Some logic to send POST request
-    router.push('/');
+    const newCourse = {
+      courseName: name.value,
+      goal: goal.value,
+      knowledge: currentSkills.value,
+      lessons: lessonsNum.value,
+      time: duration.value,
+    };
+    try {
+      await axios.post(`createCourse`, newCourse).then((response) => {
+        console.log(response);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    await router.push('/');
   } else {
     return 0;
   }
