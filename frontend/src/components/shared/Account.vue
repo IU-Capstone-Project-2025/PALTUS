4554c325164066c2f9902af3e90888987e21478f
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-07-01 00:54:48 +0300

    feat: hide links from profile tab

--- frontend/src/components/shared/Account.vue
+++ frontend/src/components/shared/Account.vue
@@ -2,12 +2,15 @@
 import AccountLink from "@/components/shared/AccountLink.vue";
 import {accountLinks} from "@/constants/accountLinks.js";
 import ProfileTab from "@/components/shared/ProfileTab.vue";
+import {ref} from "vue";
+
+let showLinks = ref(true);
 </script>
 
 <template>
-  <ProfileTab />
+  <ProfileTab @click="showLinks = !showLinks" />
   <ul>
-    <li v-for="accountLink in accountLinks">
+    <li v-for="accountLink in accountLinks" v-if="showLinks">
       <AccountLink :title="accountLink.title" :link="accountLink.link" />
     </li>
   </ul>
