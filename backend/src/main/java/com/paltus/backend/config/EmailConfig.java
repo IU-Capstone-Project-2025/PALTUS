b7442300c6735ed8f4b35ccf1474c73c5d284798
Aidar <aidarsarvartdinov2004@gmail.com>
2025-07-01 17:06:37 +0300

    feat: add EmailConfig

---
+++ backend/src/main/java/com/paltus/backend/config/EmailConfig.java
@@ -0,0 +1,34 @@
+package com.paltus.backend.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+
+public class EmailConfig {
+    @Value("${spring.mail.username}")
+    private String emailUsername;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailsender = new JavaMailSenderImpl();
+        mailsender.setHost("smtp.gmail.com");
+        mailsender.setPort(587);
+        mailsender.setUsername(emailUsername);
+        mailsender.setPassword(password);
+
+        Properties props = mailsender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailsender;
+    }
+}
