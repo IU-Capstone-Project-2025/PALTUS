5b5b71d83f71a9868e7a874d01655e8b6d3975f7
Aidar <aidarsarvartdinov2004@gmail.com>
2025-07-01 13:08:09 +0300

    feat: add User model

---
+++ backend/src/main/java/com/paltus/backend/model/User.java
@@ -0,0 +1,37 @@
+package com.paltus.backend.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private boolean enabled;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    @Column(name = "verification_expiration")
+    private LocalDateTime verificationCodeExpiresAt;
+}
