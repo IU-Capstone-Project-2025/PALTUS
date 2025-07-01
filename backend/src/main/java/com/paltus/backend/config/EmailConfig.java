d58274c49ae5d5002f2a0d5941b8eb14d5b2c976
Aidar <aidarsarvartdinov2004@gmail.com>
2025-07-01 17:09:14 +0300

    feat: add EmailService

--- backend/src/main/java/com/paltus/backend/config/EmailConfig.java
+++ backend/src/main/java/com/paltus/backend/config/EmailConfig.java
@@ -4,10 +4,11 @@ import java.util.Properties;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-
+@Configuration
 public class EmailConfig {
     @Value("${spring.mail.username}")
     private String emailUsername;
