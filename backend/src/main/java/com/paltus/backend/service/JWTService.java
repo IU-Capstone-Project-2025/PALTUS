e5ce91acef36fa54a1cbac3ec1b175bafd7187b7
Aidar <aidarsarvartdinov2004@gmail.com>
2025-07-01 15:56:07 +0300

    feat: add JWTService

---
+++ backend/src/main/java/com/paltus/backend/service/JWTService.java
@@ -0,0 +1,89 @@
+package com.paltus.backend.service;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JWTService {
+
+    private String secretKey = "";
+    private long expirationTime = 100*60*30; // 30 minutes
+
+    public JWTService() throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+        SecretKey key = keyGen.generateKey();
+        secretKey = Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public String generateToken(String username) {
+        // claims - user information
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .claims()
+                .add(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .and()
+                .signWith(getKey()) // signature for verifying the authenticity of the token
+                .compact();
+
+    }
+
+    private SecretKey getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    // extract different data from JWT token based on resolver 
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token) 
+                .getPayload(); // getting User info
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractEmail(token); //email as username
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+}
