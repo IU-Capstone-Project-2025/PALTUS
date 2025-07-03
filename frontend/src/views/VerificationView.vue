cdd578be6eb82f44db454df97dfb236093efd9a0
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-07-04 00:29:42 +0300

    feat: verification view layout

---
+++ frontend/src/views/VerificationView.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+import Logo from '../components/shared/Logo.vue'
+import ButtonGreen from "@/components/shared/ButtonGreen.vue";
+import BaseInput from "@/components/shared/BaseInput.vue";
+
+const ver_code = ref('');
+const auth = useAuthStore();
+const router = useRouter();
+
+const checkCode = async () => {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <Logo />
+    <form @submit.prevent="checkCode">
+      <h3>Check your email</h3>
+      <BaseInput
+          v-model="ver_code"
+          placeholder="Pass verification code here"
+          class="custom-input"
+      />
+
+      <ButtonGreen type="submit" title="Submit" />
+      <p>I did not receive the code</p>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+h3 {
+  font-size: 1.75rem;
+  font-weight: 700;
+  color: #F5F7FA;
+  text-align: center;
+  margin-bottom: 5vh;
+}
+form {
+  display: flex;
+  box-sizing: border-box;
+  flex-direction: column;
+  align-items: center;
+  background-color: #42A5F5;
+  padding: 3vh 1vw;
+  margin-top: 10vh;
+  border-radius: 16px;
+}
+.custom-input {
+  height: 8vh;
+  font-size: 1.2rem;
+  margin-bottom: 3vh;
+}
+
+p {
+  font-size: 1rem;
+  color: #BBDEFB;
+  margin-top: 5vh;
+  margin-bottom: 1vh;
+  cursor: pointer;
+  text-decoration: underline;
+}
+p:hover {
+  color: #F5F7FA;
+}
+</style>
