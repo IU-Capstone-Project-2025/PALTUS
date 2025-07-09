11d2a00d2d86999812a5991358aa3fe8413dec9e
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-07-09 20:45:32 +0300

    feat: finished modal layout

--- frontend/src/components/course_creation/EditCourseModal.vue
+++ frontend/src/components/course_creation/EditCourseModal.vue
@@ -17,20 +17,25 @@ const prompt = ref('');
 <template>
   <div class="modal-overlay">
     <div class="modal-content">
-      <BaseHeader :text="'You can change a course ' + course.course_name + ' adding new lesson or subtopic'" class="course-name" />
-      <ul>
-        <li v-for="lesson in course.lessons">
-          <p> {{ lesson.lesson_number }}. {{ lesson.title }}</p>
-          <ul>
-            <li v-for="subtopic in lesson.subtopics">
-              <p> {{ subtopic.topic }}</p>
-            </li>
-          </ul>
-        </li>
-      </ul>
-      <BaseInput placeholder="Prompt for changing a course" model-value="" />
-      <ButtonGreen title="Save Course" />
-      <ButtonGreen title="Cancel Course" />
+      <BaseHeader text="You can edit a course by adding a new lesson or a subtopic" />
+      <div class="modal-body">
+        <div class="course-name">
+          <BaseHeader :text="course.course_name" style="margin-bottom: 2vh" />
+        </div>
+        <ul>
+          <li v-for="lesson in course.lessons">
+            <p style="font-weight: bold"> {{ lesson.lesson_number }}. {{ lesson.title }}</p>
+            <ul>
+              <li v-for="subtopic in lesson.subtopics">
+                <p> {{ subtopic.topic }}</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <BaseInput placeholder="Prompt for changing a course" :model-value="prompt" class="modal-prompt" />
+      <ButtonGreen title="Save Course" class="modal-btn" />
+      <ButtonGreen title="Cancel Course" class="modal-btn" />
     </div>
   </div>
 </template>
@@ -41,32 +46,60 @@ const prompt = ref('');
   top: 0; left: 0;
   width: 100vw; height: 100vh;
   background: rgba(0,0,0,0.5);
-  display: flex; justify-content: center; align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   z-index: 5;
 }
 
 .modal-content {
   background: #F5F7FA;
+  box-sizing: border-box;
   padding: 3vh 2vw;
   border-radius: 10px;
-  width: 50vw;
-  height: 80vh;
-  scroll-behavior: smooth;
+  width: 60vw;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.modal-body {
+  width: 100%;
+  margin: 3vh 0 3vh 8vw;
+}
+
+.modal-prompt {
+  margin-bottom: 1vh;
 }
 
 .course-name {
+  text-transform: uppercase;
+  font-size: 2rem;
+  width: 100%;
+  text-align: left;
+  margin-top: 2vh;
+}
+
+.modal-btn {
+  width: 12vw;
+  margin: 1vh 0;
 }
 
 ul {
   list-style-type: none;
+  text-align: left;
+  padding: 0;
+}
+
+ul ul {
+  padding: 0 2vw;
 }
 
 p {
   color: #0D47A1;
   font-size: 1rem;
-}
-
-.course-name {
-  text-align: center;
 }
 </style>
