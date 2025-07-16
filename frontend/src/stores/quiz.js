abba10748ecb3b078f8fc120b07c76241ed56a15
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2025-07-16 19:43:20 +0300

    chore: add quiz store

---
+++ frontend/src/stores/quiz.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import axios from "@/plugins/axios.js";
+
+export const useQuizStore = defineStore('quiz', {
+    state: () => ({
+        quizTitle: '',
+        questions: [],
+        correctAnswers: [],
+        userAnswers: [],
+    }),
+    actions: {
+        async loadQuiz(lessonId) {
+            const quiz = axios.get(`/quiz/${lessonId}`);
+            this.quizTitle = quiz.title;
+            const newQuestions = [];
+            const correct = [];
+            for (const question of quiz.questions) {
+                const newQuestion = {};
+                newQuestion.questionId = question.questionId
+                newQuestion.questionText = question.questionText;
+                newQuestion.options = question.options;
+                correct.push(question.correctAnswer)
+
+                newQuestions.push(newQuestion);
+            }
+            this.questions = newQuestions;
+            this.correctAnswers = correct;
+        },
+        checkAnswers(answer) {
+
+        },
+    },
+});
